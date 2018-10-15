8b66674a3a7eaf862c380106e1c8a931f3e512d3
mateus-araujo <mateus_1542@hotmail.com>
2018-10-15 15:51:47 -0300

    Api service improved

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,17 @@
 import axios from 'axios'
+require('dotenv').config()
+
+let baseURL = ''
+
+if (process.env.NODE_ENV === 'development')
+  baseURL = 'http://localhost:3333'
+else
+  baseURL = 'https://odonto-back.herokuapp.com'
+
+console.log(process.env.NODE_ENV)
 
 const api = axios.create({
-  baseURL: 'http://localhost:3333',
-  // baseURL: 'https://odonto-back.herokuapp.com',
+  baseURL
 })
 
 export default api
